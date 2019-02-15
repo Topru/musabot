5fa6e412606ef2f6928e9a2b56dd60bee622a9cd
Topias <topias.jappila@gmail.com>
2019-02-16 01:21:02 +0200

    added example config

---
+++ config.example.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  // Your bot name. Typically, this is your bot's username without the discriminator.
+  // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
+  name: "musabot",
+
+  // The bot's command prefix. The bot will recognize as command any message that begins with it.
+  // i.e: "-wiseoldman foo" will trigger the command "foo",
+  //      whereas "WiseOldMan foo" will do nothing at all.
+  prefix:  "-asd",
+
+  // Your bot's user token. If you don't know what that is, go here:
+  // https://discordapp.com/developers/applications/me
+  // Then create a new application and grab your token.
+  token: "NTI5NTEzOTM5ODUyMDY2ODE3.Dwx8zQ.dSVOnL0CU8U7ulsAWEL-ny6LIKE",
+
+  // If this option is enabled, the bot will delete the message that triggered it, and its own
+  // response, after the specified amount of time has passed.
+  // Enable this if you don't want your channel to be flooded with bot messages.
+  // ATTENTION! In order for this to work, you need to give your bot the following permission:
+  // MANAGE_MESSAGES - 	0x00002000
+  // More info: https://discordapp.com/developers/docs/topics/permissions
+  deleteAfterReply: {
+    enabled: false,
+    time: 10000, // In milliseconds
+  },
+
+  youtubeToken: "AIzaSyCt6TQ9AKUMdIhJzvmtl1MTajgd7mn15FI",
+
+  dataStoreCredentials: "./credentials/musabot.json"
+};
